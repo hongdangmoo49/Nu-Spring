4823e41a3549bf9d64243990f81cb9f7d550b454
hongdangmoo49 <118052344+hongdangmoo49@users.noreply.github.com>
2022-11-30 04:05:25 +0900

    Add files via upload

---
+++ Blog/springnoob/src/main/java/com/sparta/springnoob/dto/BlogRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.springnoob.dto;
+
+import lombok.Getter;
+
+@Getter
+public class BlogRequestDto {
+    private String username;
+    private String contents;
+    private String title;
+    private String password;
+}
+
---
+++ Blog/springnoob/src/test/java/com/sparta/springnoob/SpringnoobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springnoob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringnoobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
